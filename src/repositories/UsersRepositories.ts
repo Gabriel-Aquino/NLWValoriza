71c91b29c00c7a3322ee55ce18aed9c500d38530
Gabriel-Aquino <gabrielllpk@gmail.com>
2021-06-21 22:02:53 -0300

    class-02: creation of services, controllers and routes of Users and code refactoring of service.ts

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { User } from '../src/entities/User';
+import { User } from '../entities/User';
 
 @EntityRepository(User)
 class UsersRepositories extends Repository<User> {
